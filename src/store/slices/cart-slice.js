944be0b9dde21a35c86e94102822d7e50bab1419
Dipankar <dipankarcode@gmail.com>
2023-04-12 23:07:17 +0530

    setting cart slice

--- src/store/slices/cart-slice.js
+++ src/store/slices/cart-slice.js
@@ -3,33 +3,36 @@ import { createSlice } from "@reduxjs/toolkit";
 const cart = createSlice({
     name : "cart" , 
     initialState : { 
-        items : new Map() ,
-        totalQuantity  : 0 
+        itemsCountMap: new Map() ,
+        itemMap : new Map() 
      } , 
     reducers : {
         addItemToCart(state , action) {
             const item = action.payload ;
 
-            let countOfItem = state.items.get(item) ?? 0 ; 
+            // { itemid : "dsgdsg" , name : "OK" , price : "034"} ;
 
-            state.totalQuantity -= countOfItem ;
+            const { id  } = item ;
+            state.itemMap.set(id,item) ;
+
+            let countOfItem = state.itemsCountMap.get(id) ?? 0  ;  
             countOfItem++ ;
-            state.totalQuantity += countOfItem ;
-
-
-            state.items.set(item , countOfItem ) ;
+            state.itemsCountMap.set(id , countOfItem ) ;
             
         },
         removeItemFromCart(state , action){
             const item = action.payload ;
-            let countOfItem = state.items.get(item) ?? 0 ; 
 
-            state.totalQuantity -= countOfItem ;
+            const { id } = item ;
+            let countOfItem = state.itemsCountMap.get(id) ?? 0 ; 
+
+
             countOfItem-- ; 
             countOfItem = Math.max(countOfItem,0) ;
 
-            state.totalQuantity += countOfItem ;
-            state.items.set(item , countOfItem ) ;
+            state.itemsCountMap.set(id , countOfItem ) ;
+
+            if(countOfItem === 0) state.itemsCountMap.delete(id) ;
         }
     }
 })
